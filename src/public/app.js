6d27ef1778be3d2834774773c817062524361926
hdddhdd <nuly08@naver.com>
2022-05-13 00:34:15 +0900

    #1.5 Recap

--- src/public/app.js
+++ src/public/app.js
@@ -1,18 +1,19 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-
-socket.addEventListener("open", () => {
+function handleOpen() {
     console.log("Connected to Server ✅");
-  });
-  
-  socket.addEventListener("message", (message) => {
+}
+socket.addEventListener("open", handleOpen);
+
+
+socket.addEventListener("message", (message) => {
     console.log("New message: ", message.data);
-  });
-  
-  socket.addEventListener("close", () => {
+});
+
+socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
-  });
-  
-  setTimeout(() => {
+});
+
+setTimeout(() => {
     socket.send("hello from the browser!");
-  }, 10000);
+}, 10000);
