9ad73ca69a653239f9ca2773f33a4a441bd81f55
hdddhdd <nuly08@naver.com>
2022-05-13 09:48:37 +0900

    #2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,44 +1,39 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+  });
+// const sockets = [];
 
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
 
-const sockets = [];
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach((aSocket) =>
+//                     aSocket.send(`${socket.nickname}: ${message.payload}`)
+//                 );
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//     });
+// });
 
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
